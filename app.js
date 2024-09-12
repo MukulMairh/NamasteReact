2d4e429bf301a4e6c09496398471fdeb46bb7600
Mukul <msoni7718@gmail.com>
2024-09-12 23:22:37 +0530

    Changes in package.json, React version change, removed main:App.js to avoid conflict with parcel, Added Mock designing where we will be having AppLayout that will contain different components on high level those will be head, body and footer

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
 //element, attributes , what message it is going to hold/ print 
-const heading = React.createElement("h1", { id: "heading" }, "Hello World From React "); //core
+// const heading = React.createElement("h1", { id: "heading" }, "Hello World From React "); //core
 const root = ReactDOM.createRoot(document.getElementById("root")); //Dom lib
 
-//this will return you an object. This will not return you an element. 
-//React create element will return you an object not element. 
-//look at console, you will the type of element created, also props. 
-//Props part, id = heading, children : "Hello World From React"
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://w7.pngwing.com/pngs/524/137/png-transparent-restaurant-logo-illustration-gluten-free-diet-logo-celiac-disease-wheat-restaurant-logo-emblem-food-free-logo-design-template.png"></img>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+        </div>
+    )
+}
 
 
+// console.log(heading);
+// root.render(heading);//To render it on browser
 
+// console.log(parent);
 
-
-/**
- * <div id="parent">
- *      <div id="child">
- *          <h1></h1>
- *      </div>
- * </div>
- */
-
-//How to make same structure in react 
-const parent = React.createElement("div", { id: "parent" },
-    React.createElement("div", { id: "child" }, React.createElement("h1", {}, "I am h1 tag"))
-);
-
-console.log(heading);
-root.render(heading);//To render it on browser
-
-console.log(parent);
-root.render(parent);//To render it on browser
+root.render(<AppLayout />);//To render it on browser
