fa8b1b74fef9af8e23e7eec794d9cdb59f058c81
Mukul <msoni7718@gmail.com>
2025-01-23 23:24:49 +0530

    Started food delivery application, learnt website structure

--- app.js
+++ app.js
@@ -23,11 +23,47 @@ const Header = () => {
 
     )
 }
+//For inline styling
+const styleCard = {
+    backgroundColor: "#f0f0f0"
+}
+
+const RestrauntCard = () => {
+    return (
+        <div className="res-card" style={styleCard}>
+            <div className="res-image-container">
+                {/* Here goes image */}
+                <img className="res-logo" alt="res-logo" src="https://w7.pngwing.com/pngs/524/137/png-transparent-restaurant-logo-illustration-gluten-free-diet-logo-celiac-disease-wheat-restaurant-logo-emblem-food-free-logo-design-template.png"></img>
+            </div>
+            <div className="res-name">
+                <h3>Meghna Foods</h3>
+            </div>
+            <h4>Biryani, Nort India, Asian </h4>
+            <h4>4.4 stars</h4>
+            <h4>38 mins</h4>
+
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                {/* Here goes many restraunt cards */}
+                <RestrauntCard />
+            </div>
+        </div>
+
+    )
+}
 
 const AppLayout = () => {
     return (
         <div className="app">
             <Header />
+            <Body />
         </div>
     )
 }
